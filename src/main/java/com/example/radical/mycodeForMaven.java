006a2b49428f8460b32cb281bbbbd1ac462de525
devopsMayankDubey <mayankdevops001@gmail.com>
2023-07-11 23:40:46 -0700

    added one class called mycodeForMaven

---
+++ src/main/java/com/example/radical/mycodeForMaven.java
@@ -0,0 +1,12 @@
+package com.example.radical;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mycodeForMaven {
+    @GetMapping("/mycodeForMaven")
+    public String getData() {
+        return "This is just mycode for maven";
+    }
+}
