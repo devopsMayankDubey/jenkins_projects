a715d019fbc0f5ee8ef870bc9901e01514c98a4e
devopsMayankDubey <mayankdevops001@gmail.com>
2023-07-13 00:31:18 -0700

    we

--- src/main/java/com/example/radical/mycodeForMaven.java
+++ src/main/java/com/example/radical/mycodeForMaven.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class mycodeForMaven {
     @GetMapping("/mycodeForMaven")
     public String getData() {
-        return "just mycode  by webhook again";
+        return "just mycode  by  again";
     }
 }
