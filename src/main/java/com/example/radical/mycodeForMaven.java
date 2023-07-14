84605bef14a2d30863342de929ad72ac0742fafb
devopsMayankDubey <mayankdevops001@gmail.com>
2023-07-14 05:38:04 -0700

    allgood

--- src/main/java/com/example/radical/mycodeForMaven.java
+++ src/main/java/com/example/radical/mycodeForMaven.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class mycodeForMaven {
     @GetMapping("/mycodeForMaven")
     public String getData() {
-        return "This ise by  again";
+        return "This  by  again";
     }
 }
